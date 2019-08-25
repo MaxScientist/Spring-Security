4c8fa846e76831fb269032c327fb5196676a4382
Max Galymzhan <maksatgalymzhan@gmail.com>
2019-08-25 11:21:39 +0600

    Testing

---
+++ src/test/java/org/doit/util/RepoTest.java
@@ -0,0 +1,42 @@
+package org.doit.util;
+
+import org.doit.config.CoreConfig;
+import org.doit.config.PersistenceConfig;
+import org.doit.config.SecurityConfig;
+import org.doit.config.WebConfig;
+import org.doit.model.Address;
+import org.doit.model.User;
+import org.doit.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        WebConfig.class, PersistenceConfig.class,
+        CoreConfig.class, SecurityConfig.class
+})
+@WebAppConfiguration
+public class RepoTest {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    public void testForRepo(){
+        Address address = new Address();
+        address.setCity("oekkgep");
+        address.setStreet("ekgopke");
+        User user = new User();
+        user.setId(17L);
+        user.setName("ogerpo");
+        user.setSurname("egjepgje");
+        user.setEmail("10-kf-2@gm.com");
+        user.setPassword("rogpejgpo");
+        user.setAddress(address);
+        repository.save(user);
+    }
+}
