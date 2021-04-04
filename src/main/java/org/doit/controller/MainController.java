88d6c73df0131ae53e1eb9d2aebc5c6f39e36105
Maxis <33370157+MaxScientist@users.noreply.github.com>
2021-04-04 22:09:39 +0600

    Update MainController.java

--- src/main/java/org/doit/controller/MainController.java
+++ src/main/java/org/doit/controller/MainController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * @author Aidar Shaifutdinov.
- */
 @Controller
 public class MainController {
 
